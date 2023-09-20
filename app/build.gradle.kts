299e5b7560d1c0d2fc92c7a32b3b338ef4d479d9
Teodora Kocic <teodora.kocic@nissatech.com>
2023-09-20 18:33:23 +0200

    Added mqtt broker

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation ("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.1.1")
-    implementation ("org.eclipse.paho:org.eclipse.paho.android.service:1.1.1")
     implementation ("androidx.work:work-runtime:2.7.0")
 }
